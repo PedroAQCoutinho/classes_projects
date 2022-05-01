f80b2acf57c1d5ecc274d81e1866229cac308eee
Pedro Alves Quilici Coutinho <paq.coutinho@gmail.com>
2022-05-01 18:39:51 -0300

    ANFAVEA auto production ARIMA fit

--- time_series/ANFAVEA_arima_fit.R
+++ time_series/ANFAVEA_arima_fit.R
@@ -3,7 +3,7 @@ library(car)
 library(forecast)
 library(rmarkdown)
 #load data
-anfavea.data <- read.csv2('data/automoveismontadosanfavea.csv') 
+anfavea.data <- read.csv2('time_series/data/automoveismontadosanfavea.csv') 
 colnames(anfavea.data) <- c('dates', 'auto_production', 'x')
 anfavea.data <- anfavea.data[,1:2]
 #ts trasnform
