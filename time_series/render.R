c48de9ac99bb2c8b1e8e747a6a6e624ac915e56c
Pedro Alves Quilici Coutinho <paq.coutinho@gmail.com>
2022-05-01 18:41:21 -0300

    Render rmarkdown

--- time_series/render.R
+++ time_series/render.R
@@ -6,4 +6,4 @@ rmarkdown::render('time_series/WWWusage_arima_fit.R')
 #render anfavea
 rmarkdown::render('time_series/ANFAVEA_arima_fit.R')
 rmarkdown::render('time_series/ANFAVEA_arima_fit.R', output_format = 'github_document')
-getwd()
+
